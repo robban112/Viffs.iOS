0e80a5ce671ab49c725ca92448e6b79b91cf3e0f
Robert <robbanl@kth.se>
2018-10-10 00:46:52 +0200

    signup progress

---
+++ Viffs/AWSLoginFiles/CardRegisterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CardRegisterViewController.swift
+//  Viffs
+//
+//  Created by Robert Lorentz on 2018-10-09.
+//  Copyright © 2018 Viffs. All rights reserved.
+//
+
+import UIKit
+
+class CardRegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
