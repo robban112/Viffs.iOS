c590729cbac4a6039adac8541642510dca490e5f
Oskar Ek <oskarek24@gmail.com>
2018-11-19 15:39:18 +0100

    Scroll functionality for receipts on main screen

--- Viffs/Scenes - Logged in/Main/MainViewController.swift
+++ Viffs/Scenes - Logged in/Main/MainViewController.swift
@@ -21,9 +21,11 @@ UIGestureRecognizerDelegate {
   let minHeightLatestReceipt: CGFloat = 230
   let maxHeightLatestReceipt: CGFloat = 400
   var maximized: Bool = false
+  var receiptTableViewScrollIndex: CGFloat = 0
+  var maxReceiptTableViewHeight: CGFloat = 0
   
-    @IBOutlet var tableToSuperviewConstraint: NSLayoutConstraint!
-    @IBOutlet var butikerLabel: UILabel!
+  @IBOutlet var tableToSuperviewConstraint: NSLayoutConstraint!
+  @IBOutlet var butikerLabel: UILabel!
   @IBOutlet var kvittoLabel: UILabel!
   @IBOutlet var latestReceiptSearchBar: UISearchBar!
   @IBOutlet var latestReceiptView: UIView!
@@ -31,8 +33,8 @@ UIGestureRecognizerDelegate {
   @IBOutlet var storeButton: UIButton!
   @IBOutlet var receiptTableView: UITableView!
   
-    @IBOutlet var arrowUpButton: UIButton!
-    @IBOutlet var latestReceiptViewHeightConstraint: NSLayoutConstraint!
+  @IBOutlet var arrowUpButton: UIButton!
+  @IBOutlet var latestReceiptViewHeightConstraint: NSLayoutConstraint!
   
   @IBAction func receiptButtonPushed(_ sender: Any) {
     SceneCoordinator.shared.transition(to: Scene.mittViffs(.init()))
@@ -47,11 +49,34 @@ UIGestureRecognizerDelegate {
       maximizeLatestReceiptView()
     }
   }
+  
+  enum ScrollState {
+    case maximizedAndAtTop
+    case maximized
+    case minimized
+  }
+  
+  private var currentScrollState: ScrollState = .minimized {
+    didSet {
+      switch currentScrollState {
+      case .maximizedAndAtTop:
+        //Only scroll down
+        receiptTableView.isScrollEnabled = false
+        break
+      case .maximized:
+        receiptTableView.isScrollEnabled = true
+        break
+      case .minimized:
+        receiptTableView.isScrollEnabled = false
+        break
+      }
+    }
+  }
     
     
   @objc func reloadTable() {
     receiptTableView.reloadData()
-
+    maxReceiptTableViewHeight = receiptTableView.rowHeight * CGFloat(Current.receipts.count)
   }
   
   func setObservers() {
@@ -64,6 +89,8 @@ UIGestureRecognizerDelegate {
     receiptTableView.delegate = self
     receiptTableView.dataSource = self
     
+    maxReceiptTableViewHeight = receiptTableView.rowHeight * CGFloat(Current.receipts.count)
+    
     latestReceiptSearchBar.delegate = self
     latestReceiptViewHeightConstraint.constant = minHeightLatestReceipt
     
@@ -72,8 +99,9 @@ UIGestureRecognizerDelegate {
     latestReceiptView.addGestureRecognizer(panRecognizer)
     setObservers()
     setupSideMenu()
-    addCard = addButton()
-    navigationItem.setRightBarButton(addCard, animated: false)
+    
+    toggleNavButton()
+
   }
   
   func setupSideMenu() {
@@ -82,6 +110,15 @@ UIGestureRecognizerDelegate {
     moreVC.title = ""
     let navRight = UISideMenuNavigationController(rootViewController: moreVC)
     SideMenuManager.default.menuRightNavigationController = navRight
+  }
+  
+  func toggleNavButton() {
+    if !maximized {
+      addCard = addButton()
+      navigationItem.setRightBarButton(addCard, animated: true)
+    } else {
+      navigationItem.setRightBarButton(nil, animated: true)
+    }
   }
   
   func addButton() -> UIBarButtonItem {
@@ -100,10 +137,12 @@ UIGestureRecognizerDelegate {
   func maximizeLatestReceiptView() {
     let screenSize: CGRect = UIScreen.main.bounds
     maximized = true
+    currentScrollState = .maximized
     self.latestReceiptViewHeightConstraint.constant = screenSize.height - 100
     UIView.animate(withDuration: 0.35) { () -> Void in
       self.addCard.isEnabled = false
       self.addCard.tintColor = UIColor.clear
+      self.toggleNavButton()
       self.latestReceiptSearchBar.isHidden = false
       self.tableToSuperviewConstraint.constant = 60
       self.arrowUpButton.setBackgroundImage(#imageLiteral(resourceName: "arrow-down-2"), for: .normal)
@@ -114,10 +153,10 @@ UIGestureRecognizerDelegate {
   
   func minimizeLatestReceiptView() {
     maximized = false
+    currentScrollState = .minimized
     self.latestReceiptViewHeightConstraint.constant = minHeightLatestReceipt
     UIView.animate(withDuration: 0.35) { () -> Void in
-      self.addCard.isEnabled = true
-      self.addCard.tintColor = nil
+      self.toggleNavButton()
       self.latestReceiptSearchBar.isHidden = true
       self.tableToSuperviewConstraint.constant = 25
       self.arrowUpButton.setBackgroundImage(#imageLiteral(resourceName: "arrow-up-2"), for: .normal)
@@ -143,25 +182,59 @@ UIGestureRecognizerDelegate {
     kvittoLabel.alpha = alpha
   }
   
+  func updateCurtainPosition(newLocation: CGFloat, diff: CGFloat) {
+    latestReceiptViewHeightConstraint.constant += diff
+    swipeCoordLoc = newLocation
+  }
+  
+//  func handleReceiptTableViewScroll(diff: CGFloat) {
+//    if diff > 0 {
+//      if receiptTableViewScrollIndex < maxReceiptTableViewHeight {
+//        receiptTableViewScrollIndex += diff
+//      }
+//    } else {
+//      receiptTableViewScrollIndex += diff
+//    }
+//    //print(receiptTableViewScrollIndex)
+//    if receiptTableViewScrollIndex < 10 {
+//      currentScrollState = .maximizedAndAtTop
+//    }
+//  }
+  
   func gestureChanged(translation: CGPoint) {
     let diff = swipeCoordLoc - translation.y
+    print()
     if latestReceiptViewHeightConstraint.constant >= minHeightLatestReceipt {
-      if latestReceiptViewHeightConstraint.constant + diff < minHeightLatestReceipt {
-        latestReceiptViewHeightConstraint.constant = minHeightLatestReceipt
-      } else {
-        latestReceiptViewHeightConstraint.constant += diff
-        swipeCoordLoc = translation.y
+
+      switch currentScrollState {
+      case .maximizedAndAtTop:
+        if diff <= 0 {
+          updateCurtainPosition(newLocation: translation.y, diff: diff)
+        } else if latestReceiptViewHeightConstraint.constant == UIScreen.main.bounds.height-100 {
+          currentScrollState = .maximized
+        }
+      case .maximized:
+        break
+        //handleReceiptTableViewScroll(diff: diff)
+        //updateCurtainPosition(newLocation: translation.y, diff: diff)
+      case .minimized:
+        print(diff)
+        if diff > 0 { updateCurtainPosition(newLocation: translation.y, diff: diff) }
+        else if latestReceiptViewHeightConstraint.constant > minHeightLatestReceipt { updateCurtainPosition(newLocation: translation.y, diff: diff) }
       }
+      
       
       if latestReceiptViewHeightConstraint.constant > minHeightLatestReceipt && latestReceiptViewHeightConstraint.constant < maxHeightLatestReceipt {
         setAlphaOnButtonsAccordingToLatestReceiptHeight(height: latestReceiptViewHeightConstraint.constant)
       }
+    } else {
+      latestReceiptViewHeightConstraint.constant = minHeightLatestReceipt
     }
   }
   
   func gestureEnded(verticalVelocity: CGFloat) {
     if latestReceiptViewHeightConstraint.constant > maxHeightLatestReceipt {
-      if (verticalVelocity > 0) {
+      if (verticalVelocity > 0 && currentScrollState != .maximized) {
         minimizeLatestReceiptView()
       } else {
         maximizeLatestReceiptView()
@@ -192,7 +265,7 @@ UIGestureRecognizerDelegate {
   
 }
 
-extension MainViewController: UISearchBarDelegate, UITableViewDelegate {
+extension MainViewController: UISearchBarDelegate, UITableViewDelegate, UIScrollViewDelegate {
   
   func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
     searchBar.endEditing(true)
@@ -216,5 +289,17 @@ extension MainViewController: UISearchBarDelegate, UITableViewDelegate {
     }
     tableView.deselectRow(at: indexPath, animated: true)
     tableView.deselectRow(at: indexPath, animated: true)
+  }
+  
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    if receiptTableView.isScrollEnabled {
+      let offset = scrollView.contentOffset.y
+      if offset < 0 {
+        currentScrollState = .maximizedAndAtTop
+      }
+      else if offset > 0 {
+        currentScrollState = .maximized
+      }
+    }
   }
 }
